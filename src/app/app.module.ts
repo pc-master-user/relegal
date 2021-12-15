59a68cb829695a3ab2d7ee625a0b12eede8a740b
pc-master-user <prabhakarchowdary777@gmail.com>
2021-12-16 00:52:31 +0530

    Adding firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DropdownModule} from "primeng/dropdown";
 import {SharedModule} from "./shared/shared.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,11 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'clinix-dev-1488b',
+    appId: '1:835438389301:web:cc4f1f53d6e345c624297c',
+    storageBucket: 'clinix-dev-1488b.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyBpY6zFOOMuF7n2UyTab3z0nkVLjId687Q',
+    authDomain: 'clinix-dev-1488b.firebaseapp.com',
+    messagingSenderId: '835438389301',
+    measurementId: 'G-XNTEXDYGV7',
+  },
   production: false
 };
 
