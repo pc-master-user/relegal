ef19fb63650e272759db8fb0d027801d15c53e91
puti323 <f20140323h@alumni.bitd-pilani.ac.in>
2021-12-17 03:21:25 +0530

    Updated User management page

--- src/app/login/users/users.component.ts
+++ src/app/login/users/users.component.ts
@@ -4,7 +4,19 @@ import {AuthService} from "../auth.service";
 import {Subject} from "rxjs";
 import {Router} from "@angular/router";
 import {Table} from "primeng/table";
-import {PrimeNGConfig} from "primeng/api";
+import {PrimeNGConfig, ConfirmationService, MessageService} from "primeng/api";
+
+export interface User {
+  id?:string;
+  username?:string;
+  password?:string;
+  name?:string;
+  status?:string;
+  group?:string;
+  role?:string;
+  rememberMe?:boolean | string;
+  img?:string;
+}
 
 @Component({
   selector: 'app-users',
@@ -14,15 +26,27 @@ import {PrimeNGConfig} from "primeng/api";
 export class UsersComponent implements OnInit {
   isUserLoggedIn!: boolean;
   loading = false;
-  users!: any[];
+  users!: User[];
+  legalTypes!: any[];
+  statuses!: any[];
   private unsubscribe$ = new Subject<void>();
   @ViewChild('dt') table: Table | undefined;
   @ViewChild('globalSearch') search: ElementRef | undefined;
-  selectedUsers!: any[];
+
+
+  userDialog!: boolean;
+  user!: User;
+  selectedUsers!: User[] | null;
+
+  submitted!: boolean;
   constructor(
     private authService: AuthService,
     private router: Router,
-    private primengConfig: PrimeNGConfig) {
+    private primengConfig: PrimeNGConfig,
+    
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {
     authService.getAuthObj().pipe(takeUntil(this.unsubscribe$)).subscribe( (data: any) => {
       if (data && data?.username) {
         this.isUserLoggedIn = true;
@@ -34,19 +58,113 @@ export class UsersComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (!this.isUserLoggedIn) {
-      this.router.navigate(['/']);
-    }
+    // if (!this.isUserLoggedIn) {
+    //   this.router.navigate(['/']);
+    // }
+    this.legalTypes = [{name: 'Attorney', code: 'atr'}, {name: 'Paralegal', code: 'par'}, {name: 'Admin', code: 'admin'}]
+    this.statuses = [{name: 'Active', code: 'act'}, {name: 'Inactive', code: 'in'}]
     this.users = [
-      {name: 'Ian Chesnut',username: 'ian.chesnut@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Active', rememberMe: true, img: 'profileImage.png'},
-      {name: 'Faith Robinson', username: 'faith.robinson@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Inactive', rememberMe: false, img: 'profileImage.png'},
-      {name: 'Track Aksam',username: 'track.aksam@gmail.com', password: 'Test@123', role: 'Admin', status: 'Active', rememberMe: true, img: 'profileImage.png'},
-      {name: 'Scot Walter',username: 'scot.walter@gmail.com', password: 'Test@123', role: 'Admin', status: 'Active', rememberMe: false, img: 'profileImage.png'},
-      {name: 'Chris Bowen',username: 'chris.bowen@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Inactive', rememberMe: true, img: 'profileImage.png'},
-      {name: 'Dan Spanser',username: 'dan.spanser@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Active', rememberMe: true, img: 'profileImage.png'},
-      {name: 'Erin Holland',username: 'erin.holland@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Inactive', rememberMe: true, img: 'profileImage.png'},
-      {name: 'Toney Gabriel',username: 'toney.gabriel@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Active', rememberMe: false, img: 'profileImage.png'}
+      {"id": "1000", name: 'Ian Chesnut',username: 'ian.chesnut@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Active', group: 'alpha', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1001", name: 'Faith Robinson', username: 'faith.robinson@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Inactive', group: 'alpha', rememberMe: false, img: 'profileImage.png'},
+      {"id": "1002", name: 'Track Aksam',username: 'track.aksam@gmail.com', password: 'Test@123', role: 'Admin', status: 'Active', group: 'alpha', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1003", name: 'Scot Walter',username: 'scot.walter@gmail.com', password: 'Test@123', role: 'Admin', status: 'Active', group: 'beta', rememberMe: false, img: 'profileImage.png'},
+      {"id": "1004", name: 'Chris Bowen',username: 'chris.bowen@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Inactive', group: 'alpha', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1005", name: 'Dan Spanser',username: 'dan.spanser@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Active', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1006", name: 'Erin Holland',username: 'erin.holland@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Inactive', group: 'beta', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1007", name: 'Toney Gabriel',username: 'toney.gabriel@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Active', group: 'beta', rememberMe: false, img: 'profileImage.png'},
+      {"id": "1008", name: 'Ian Chesnut',username: 'ian.chesnut@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Active', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1009", name: 'Faith Robinson', username: 'faith.robinson@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Inactive', group: 'sigma', rememberMe: false, img: 'profileImage.png'},
+      {"id": "1010", name: 'Track Aksam',username: 'track.aksam@gmail.com', password: 'Test@123', role: 'Admin', status: 'Active', group: 'sigma', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1011", name: 'Scot Walter',username: 'scot.walter@gmail.com', password: 'Test@123', role: 'Admin', status: 'Active', group: 'sigma', rememberMe: false, img: 'profileImage.png'},
+      {"id": "1012", name: 'Chris Bowen',username: 'chris.bowen@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Inactive', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1013", name: 'Dan Spanser',username: 'dan.spanser@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Active', group: 'sigma', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1014", name: 'Erin Holland',username: 'erin.holland@gmail.com', password: 'Test@123', role: 'Paralegal', status: 'Inactive', rememberMe: true, img: 'profileImage.png'},
+      {"id": "1015", name: 'Toney Gabriel',username: 'toney.gabriel@gmail.com', password: 'Test@123', role: 'Attorney', status: 'Active', group: 'sigma', rememberMe: false, img: 'profileImage.png'},
     ]
+  }
+
+  openNew() {
+    this.user = {};
+    this.submitted = false;
+    this.userDialog = true;
+  }
+
+  deleteSelectedUsers() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete the selected users?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.users = this.users.filter(val => this.selectedUsers && !this.selectedUsers.includes(val));
+        this.selectedUsers = null;
+        this.messageService.add({severity:'success', summary: 'Successful', detail: 'Users Deleted', life: 3000});
+      }
+    });
+  }
+
+  editUser(user: User) {
+    this.user = {...user};
+    this.userDialog = true;
+  }
+
+  deleteUser(user: User) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + user.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.users = this.users.filter(val => val.id !== user.id);
+        this.user = {};
+        this.messageService.add({severity:'success', summary: 'Successful', detail: 'User Deleted', life: 3000});
+      }
+    });
+  }
+
+  hideDialog() {
+    this.userDialog = false;
+    this.submitted = false;
+  }
+
+  saveUser() {
+    this.submitted = true;
+
+    if (this.user?.name && this.user?.name.trim()) {
+      if (this.user.id) {
+        this.users[this.findIndexById(this.user.id)] = this.user;
+        this.messageService.add({severity:'success', summary: 'Successful', detail: 'User Updated', life: 3000});
+      }
+      else {
+        this.user.id = this.createId();
+        this.user.img = 'user-placeholder.svg';
+        this.users.push(this.user);
+        this.messageService.add({severity:'success', summary: 'Successful', detail: 'User Created', life: 3000});
+      }
+
+      this.users = [...this.users];
+      this.userDialog = false;
+      this.user = {};
+    }
+  }
+
+  findIndexById(id: string): number {
+    let index = -1;
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  }
+
+  createId(): string {
+    let id = '';
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for ( var i = 0; i < 5; i++ ) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return id;
   }
 
   test(data: any): void {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,6 +10,19 @@ import {DropdownModule} from "primeng/dropdown";
 import {TableModule} from "primeng/table";
 import {MultiSelectModule} from "primeng/multiselect";
 import {ContextMenuModule} from "primeng/contextmenu";
+import {ToastModule} from 'primeng/toast';
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {ToolbarModule} from "primeng/toolbar";
+import {DialogModule} from "primeng/dialog";
+import {SliderModule} from "primeng/slider";
+import {ProgressBarModule} from "primeng/progressbar";
+import {CalendarModule} from "primeng/calendar";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {InputNumberModule} from "primeng/inputnumber";
+import {FileUploadModule} from "primeng/fileupload";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {ConfirmationService, MessageService} from "primeng/api";
+import {TabViewModule} from "primeng/tabview";
 
 const materialModules = [
   FormsModule,
@@ -22,7 +35,19 @@ const materialModules = [
   RippleModule,
   TableModule,
   MultiSelectModule,
-  ContextMenuModule
+  ContextMenuModule,
+  ToastModule,
+  ConfirmDialogModule,
+  ToolbarModule,
+  FileUploadModule,
+  DialogModule,
+  CalendarModule,
+  SliderModule,
+  ProgressBarModule,
+  RadioButtonModule,
+  InputNumberModule,
+  InputTextareaModule,
+  TabViewModule
 ];
 
 @NgModule({
@@ -35,6 +60,7 @@ const materialModules = [
   exports: [
     ...materialModules,
     AppNavbarComponent
-  ]
+  ],
+  providers: [MessageService, ConfirmationService]
 })
 export class SharedModule { }
