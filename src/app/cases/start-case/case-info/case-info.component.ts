a26087d6791514d2f2f6e605d3d4e239166c391c
pc-master-user <prabhakarchowdary777@gmail.com>
2021-12-20 20:17:19 +0530

    updating case form

--- src/app/cases/start-case/case-info/case-info.component.ts
+++ src/app/cases/start-case/case-info/case-info.component.ts
@@ -1,88 +1,145 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { County, Matter, Client,Investor,Address } from '../../case';
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  Component,
+  OnChanges,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { County, Matter, Client, Investor, Address } from '../../case';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { PrimeNGConfig, ConfirmationService, MessageService } from "primeng/api";
+import {
+  PrimeNGConfig,
+  ConfirmationService,
+  MessageService,
+} from 'primeng/api';
 
 @Component({
   selector: 'app-case-info',
   templateUrl: './case-info.component.html',
-  styleUrls: ['./case-info.component.scss']
+  styleUrls: ['./case-info.component.scss'],
 })
 export class CaseInfoComponent implements OnInit {
-   @Input() matter!: Matter;
-   constructor(private store: AngularFirestore,private primengConfig: PrimeNGConfig,
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService) { 
-    this.primengConfig.ripple = true;
+  @Input() matter!: Matter;
+  matterForm!: FormGroup;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('value changed', this.matter);
+    if (this.matter?.id) {
+      this.matterForm
+        .get('streetNumber')
+        ?.setValue(this.matter?.property?.streetNumber);
     }
+
+    // this.matterForm = new FormGroup({
+    //   streetNumber: new FormControl(this.matter?.property?.streetNumber, [
+    //     Validators.required,
+    //   ]),
+    //   streetName: new FormControl(this.matter?.property?.streetName, [
+    //     Validators.required,
+    //   ]),
+    //   city: new FormControl(
+    //     this.cities.find((city) => city.name === this.matter?.property?.city),
+    //     [Validators.required]
+    //   ),
+    //   state: new FormControl(this.matter?.property?.state, [
+    //     Validators.required,
+    //   ]),
+    //   county: new FormControl(this.matter?.property?.county, [
+    //     Validators.required,
+    //   ]),
+    //   zip: new FormControl(this.matter?.property?.zipCode, [
+    //     Validators.required,
+    //   ]),
+    //   clientCode: new FormControl(
+    //     this.clientCodes.find(
+    //       (client) => client.name === this.matter?.client?.code
+    //     ),
+    //     [Validators.required]
+    //   ),
+    //   clientLoanNumber: new FormControl(this.matter?.client?.loanNumber, [
+    //     Validators.required,
+    //   ]),
+    //   investorCode: new FormControl(
+    //     this.investorCodes.find(
+    //       (inv) => inv.name === this.matter?.investor?.code
+    //     ),
+    //     [Validators.required]
+    //   ),
+    //   investorLoanNumber: new FormControl(this.matter?.investor?.loanNumber, [
+    //     Validators.required,
+    //   ]),
+    //   caseType: new FormControl(this.matter?.caseType, [Validators.required]),
+    //   defendantName: new FormControl(this.matter?.defendantName, [
+    //     Validators.required,
+    //   ]),
+    // });
+  }
+  constructor(
+    private store: AngularFirestore,
+    private primengConfig: PrimeNGConfig,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {
+    this.primengConfig.ripple = true;
+  }
   counties: County[] = [
     this.getCounty(1, 'Albany', 3, 3),
-    this.getCounty( 2	,"Allegany",	8	,4),
-    this.getCounty(3,	"Bronx"	,12,	1),
-    this.getCounty(4,	"Broome"	,6	,3),
-    this.getCounty(5,	"Cattaraugus",	8,	4),
-    this.getCounty(6,	"Cayuga",	7	,4),
-    this.getCounty(7,	"Chautauqua",	8	,4),
-    this.getCounty(8,	"Chemung"	,6	,3),
+    this.getCounty(2, 'Allegany', 8, 4),
+    this.getCounty(3, 'Bronx', 12, 1),
+    this.getCounty(4, 'Broome', 6, 3),
+    this.getCounty(5, 'Cattaraugus', 8, 4),
+    this.getCounty(6, 'Cayuga', 7, 4),
+    this.getCounty(7, 'Chautauqua', 8, 4),
+    this.getCounty(8, 'Chemung', 6, 3),
     this.getCounty(1, 'Albany', 3, 3),
-    this.getCounty( 9	,"Chenango"	,6,	3),
+    this.getCounty(9, 'Chenango', 6, 3),
   ];
-  cities: any[] = [
-    {name: "New York", code: 'NY'},
-  ];
+  cities: any[] = [{ name: 'New York', code: 'NY' }];
   caseTypes: any[] = [
-    {name: "APPEALS", code: '1'},
-    {name: 'EVC', code: '2'},
-    {name: 'CO-OP', code: '3'},
+    { name: 'APPEALS', code: '1' },
+    { name: 'EVC', code: '2' },
+    { name: 'CO-OP', code: '3' },
     { name: 'LIT', code: '4' },
-     {name: 'REPELVIN', code: '5'},
+    { name: 'REPELVIN', code: '5' },
   ];
-  clientCodes: any[] = [
-    {name: 'NSM', code: '1'},
-  ];
+  clientCodes: any[] = [{ name: 'NSM', code: '1' }];
   investorCodes: any[] = [
     { name: 'CONV', code: '1' },
     { name: 'FHL', code: '2' },
     { name: 'VA', code: '3' },
     { name: 'FHA', code: '4' },
-    {name: 'FNM', code: '5'},
+    { name: 'FNM', code: '5' },
   ];
-  getCounty(id:number,name: string, district: number,
-    division: number) { 
+  getCounty(id: number, name: string, district: number, division: number) {
     const county: County = {
       id: id,
       district: district,
       division: division,
-      name:name
-    }
+      name: name,
+    };
     return county;
   }
-  matterForm = new FormGroup({
-    streetNumber: new FormControl('', [Validators.required]),
-    streetName: new FormControl('', [Validators.required,]),
-    city: new FormControl({}, [Validators.required,]),
-    state: new FormControl('', [Validators.required,]),
-    county: new FormControl({}, [Validators.required,]),
-    zip: new FormControl('', [Validators.required,]),
-    clientCode: new FormControl({}, [Validators.required,]),
-    clientLoanNumber: new FormControl('', [Validators.required,]),
-    investorCode: new FormControl({}, [Validators.required,]),
-    investorLoanNumber: new FormControl('', [Validators.required,]),
-    caseType: new FormControl({}, [Validators.required,]),
-    defendantName: new FormControl('', [Validators.required,]),
-  });
-  async onSubmit() { 
- 
-    console.log(this.matterForm.value)
-     const client: Client = {code: this.matterForm.get('clientCode')?.value?.name,loanNumber: this.matterForm.get('clientLoanNumber')?.value}
-    const investor: Investor = {code: this.matterForm.get('investorCode')?.value?.name,loanNumber: this.matterForm.get('investorLoanNumber')?.value}
+
+  async onSubmit() {
+    console.log(this.matterForm.value);
+    const client: Client = {
+      code: this.matterForm.get('clientCode')?.value?.name,
+      loanNumber: this.matterForm.get('clientLoanNumber')?.value,
+    };
+    const investor: Investor = {
+      code: this.matterForm.get('investorCode')?.value?.name,
+      loanNumber: this.matterForm.get('investorLoanNumber')?.value,
+    };
     const address: Address = {
       city: this.matterForm.get('city')?.value?.name,
-      county: this.matterForm.get('county')?.value, state: this.matterForm.get('state')?.value,
-      streetName: this.matterForm.get('streetName')?.value, streetNumber: this.matterForm.get('streetNumber')?.value,
-       zipCode:this.matterForm.get('zip')?.value,
-    }
+      county: this.matterForm.get('county')?.value,
+      state: this.matterForm.get('state')?.value,
+      streetName: this.matterForm.get('streetName')?.value,
+      streetNumber: this.matterForm.get('streetNumber')?.value,
+      zipCode: this.matterForm.get('zip')?.value,
+    };
     const matter: Matter = {
       id: this.store.createId(),
       caseType: this.matterForm.get('caseType')?.value?.name,
@@ -92,29 +149,72 @@ export class CaseInfoComponent implements OnInit {
       createdDate: new Date(Date.now()),
       updatedDate: new Date(Date.now()),
       property: address,
-      status: "Active",
-      isContested:false
-    }
+      status: 'Active',
+      isContested: false,
+    };
     await this.store.collection('Matters').add(matter);
-    this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Matter Created', life: 3000 });
- 
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Successful',
+      detail: 'Matter Created',
+      life: 3000,
+    });
   }
   cancel() {
-    console.log('Cancel Called')
+    console.log('Cancel Called');
     this.confirmationService.confirm({
-      message: "Current Changes will be lost",
+      message: 'Current Changes will be lost',
       header: 'Cancel',
       icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-       
-      }
+      accept: () => {},
     });
   }
 
   selectedCounty!: string;
 
-
   ngOnInit(): void {
+    this.matterForm = new FormGroup({
+      streetNumber: new FormControl(this.matter?.property?.streetNumber, [
+        Validators.required,
+      ]),
+      streetName: new FormControl(this.matter?.property?.streetName, [
+        Validators.required,
+      ]),
+      city: new FormControl(
+        this.cities.find((city) => city.name === this.matter?.property?.city),
+        [Validators.required]
+      ),
+      state: new FormControl(this.matter?.property?.state, [
+        Validators.required,
+      ]),
+      county: new FormControl(this.matter?.property?.county, [
+        Validators.required,
+      ]),
+      zip: new FormControl(this.matter?.property?.zipCode, [
+        Validators.required,
+      ]),
+      clientCode: new FormControl(
+        this.clientCodes.find(
+          (client) => client.name === this.matter?.client?.code
+        ),
+        [Validators.required]
+      ),
+      clientLoanNumber: new FormControl(this.matter?.client?.loanNumber, [
+        Validators.required,
+      ]),
+      investorCode: new FormControl(
+        this.investorCodes.find(
+          (inv) => inv.name === this.matter?.investor?.code
+        ),
+        [Validators.required]
+      ),
+      investorLoanNumber: new FormControl(this.matter?.investor?.loanNumber, [
+        Validators.required,
+      ]),
+      caseType: new FormControl(this.matter?.caseType, [Validators.required]),
+      defendantName: new FormControl(this.matter?.defendantName, [
+        Validators.required,
+      ]),
+    });
   }
-
 }
--- src/app/cases/start-case/start-case.component.ts
+++ src/app/cases/start-case/start-case.component.ts
@@ -1,24 +1,43 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
-import {MatSelectionListChange} from "@angular/material/list";
-import {MessageService, PrimeIcons, PrimeNGConfig} from "primeng/api";
-import {CourtDate} from "../case";
-import {FormControl} from "@angular/forms";
-import {AngularFirestore} from "@angular/fire/compat/firestore";
-import {ActivatedRoute} from "@angular/router";
-import {take} from "rxjs/operators";
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { MatSelectionListChange } from '@angular/material/list';
+import { MessageService, PrimeIcons, PrimeNGConfig } from 'primeng/api';
+import { CourtDate } from '../case';
+import { FormControl } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 import * as moment from 'moment';
-
 
 @Component({
   selector: 'app-start-case',
   templateUrl: './start-case.component.html',
-  styleUrls: ['./start-case.component.scss']
+  styleUrls: ['./start-case.component.scss'],
 })
 export class StartCaseComponent implements OnInit {
-  toggle = true
+  toggle = true;
   stages!: ElementRef<MatSelectionListChange>;
-  leftSideItems: string[] = ['Case Info', 'Case Progress', 'File History', 'Comments', 'Court Dates', 'Parties', 'Loan/Property Info', 'Ledger Details'];
-  rightSideItems: string[] = ['Referral Received', 'Title Report Reviewed', 'First Legal Filing', 'Service Complete', 'Settlement Released', 'OR/MSJ Entered', 'ROC Received', 'Judgement Entered', 'Sale Held', 'Post-Sale Deed'];
+  leftSideItems: string[] = [
+    'Case Info',
+    'Case Progress',
+    'File History',
+    'Comments',
+    'Court Dates',
+    'Parties',
+    'Loan/Property Info',
+    'Ledger Details',
+  ];
+  rightSideItems: string[] = [
+    'Referral Received',
+    'Title Report Reviewed',
+    'First Legal Filing',
+    'Service Complete',
+    'Settlement Released',
+    'OR/MSJ Entered',
+    'ROC Received',
+    'Judgement Entered',
+    'Sale Held',
+    'Post-Sale Deed',
+  ];
   tabs: any = {};
   rightEvents1!: any[];
   rightNavOpenStatus!: boolean;
@@ -30,7 +49,28 @@ export class StartCaseComponent implements OnInit {
   submitted!: boolean;
   selectedCourtDate!: CourtDate;
   courtDates: CourtDate[] = [];
-  courtDateTypes = [{name: 'Settlement Conference 1'},{name: 'Submit Order of Reference'},{name: 'Status Conference'},{name: 'Settlement Conference 2'},{name: 'Settlement Conference 3'},{name: 'Order of Reference'},{name: 'Motion for Summary Judgement'},{name: 'JFS'},{name: 'OTSC'},{name: 'Sale'},{name: 'Closing'},{name: 'Traverse Hearing'},{name: 'Deposition'},{name: 'Publication of Service'},{name: "Surrogate's Court"},{name: '90 Day Expiration'},{name: 'Answer Period Expiration'},{name: 'MSJ'},{name: 'Set Sale'},{name: 'HUD 1st Legal'}]
+  courtDateTypes = [
+    { name: 'Settlement Conference 1' },
+    { name: 'Submit Order of Reference' },
+    { name: 'Status Conference' },
+    { name: 'Settlement Conference 2' },
+    { name: 'Settlement Conference 3' },
+    { name: 'Order of Reference' },
+    { name: 'Motion for Summary Judgement' },
+    { name: 'JFS' },
+    { name: 'OTSC' },
+    { name: 'Sale' },
+    { name: 'Closing' },
+    { name: 'Traverse Hearing' },
+    { name: 'Deposition' },
+    { name: 'Publication of Service' },
+    { name: "Surrogate's Court" },
+    { name: '90 Day Expiration' },
+    { name: 'Answer Period Expiration' },
+    { name: 'MSJ' },
+    { name: 'Set Sale' },
+    { name: 'HUD 1st Legal' },
+  ];
   userId!: string;
   user!: any;
   cases: any;
@@ -40,98 +80,117 @@ export class StartCaseComponent implements OnInit {
     private primengConfig: PrimeNGConfig,
     private route: ActivatedRoute,
     private store: AngularFirestore
-    ) {
+  ) {
     this.primengConfig.ripple = true;
-    route.params.subscribe(params => {
+    route.params.subscribe((params) => {
       this.userId = params['id'];
-
-      this.store.collection('Matters').doc(this.userId).valueChanges().pipe(take(1)).subscribe(res =>{
-        this.user = res;
-        this.courtDates = this.user['courtDates'];
-        // this.store.Timestamp(this.courtDates[0]?.courtDate._seconds, this.courtDates[0]?.courtDate._nanoseconds).toDate();
-      });
+      this.store
+        .collection('Matters')
+        .doc(this.userId)
+        .valueChanges()
+        .pipe(take(1))
+        .subscribe((res) => {
+          this.user = res;
+          this.courtDates = this.user?.courtDates;
+          // this.store.Timestamp(this.courtDates[0]?.courtDate._seconds, this.courtDates[0]?.courtDate._nanoseconds).toDate();
+        });
     });
   }
 
   ngOnInit(): void {
     this.rightEvents1 = [
       {
-        status: "Referral Received",
-        date: "15/10/2020 10:30",
+        status: 'Referral Received',
+        date: '15/10/2020 10:30',
         icon: 1,
-        color: "#689F38"
+        color: '#689F38',
       },
       {
-        status: "Title Report Reviewed",
-        date: "15/10/2020 14:00",
+        status: 'Title Report Reviewed',
+        date: '15/10/2020 14:00',
         icon: 2,
-        color: "#689F38"
+        color: '#689F38',
       },
       {
-        status: "First Legal Filing",
-        date: "15/10/2020 16:15",
+        status: 'First Legal Filing',
+        date: '15/10/2020 16:15',
         icon: 3,
-        color: "#689F38"
+        color: '#689F38',
       },
       {
-        status: "Service Complete",
-        date: "16/10/2020 10:00",
+        status: 'Service Complete',
+        date: '16/10/2020 10:00',
         icon: 4,
-        color: "#689F38"
+        color: '#689F38',
       },
       {
-        status: "Settlement Released",
-        date: "15/10/2020 10:30",
+        status: 'Settlement Released',
+        date: '15/10/2020 10:30',
         icon: 5,
-        color: "#689F38"
+        color: '#689F38',
       },
       {
-        status: "OR/MSJ Entered",
-        date: "",
+        status: 'OR/MSJ Entered',
+        date: '',
         icon: 6,
-        color: "#bdbdbd"
+        color: '#bdbdbd',
       },
       {
-        status: "ROC Received",
-        date: "",
+        status: 'ROC Received',
+        date: '',
         icon: 7,
-        color: "#bdbdbd"
+        color: '#bdbdbd',
       },
       {
-        status: "Judgement Entered",
-        date: "",
+        status: 'Judgement Entered',
+        date: '',
         icon: 8,
-        color: "#bdbdbd"
+        color: '#bdbdbd',
       },
       {
-        status: "Sale Held",
-        date: "",
+        status: 'Sale Held',
+        date: '',
         icon: 9,
-        color: "#bdbdbd"
+        color: '#bdbdbd',
       },
       {
-        status: "Post-Sale Deed",
-        date: "",
+        status: 'Post-Sale Deed',
+        date: '',
         icon: 10,
-        color: "#bdbdbd"
-      }
+        color: '#bdbdbd',
+      },
     ];
     this.tabs = {
-      'Referral Received': ['Review', 'SCRA Search', 'Pacer Search', 'Title Report Order'],
+      'Referral Received': [
+        'Review',
+        'SCRA Search',
+        'Pacer Search',
+        'Title Report Order',
+      ],
       'Title Report Reviewed': ['Review', 'Intake', 'Title Issue'],
-      'First Legal Filing': ['OCA/SOR', 'First Legal Filing', 'Precomplaint SCRA', 'Precomplaint Pacer Search'],
-      'Service Complete': ['Service of Complaint', 'TTA Expiration Date', 'Publication of Service', 'Death Certificate Requested'],
+      'First Legal Filing': [
+        'OCA/SOR',
+        'First Legal Filing',
+        'Precomplaint SCRA',
+        'Precomplaint Pacer Search',
+      ],
+      'Service Complete': [
+        'Service of Complaint',
+        'TTA Expiration Date',
+        'Publication of Service',
+        'Death Certificate Requested',
+      ],
       'Settlement Conf.Released': ['RJI', 'FSC Released', 'First FSC'],
       'OR/MSJ Entered': ['Oor Prepared', 'FSC Released', 'First FSC'],
       'ROC Received': ['RJI', 'FSC Released', 'First FSC'],
       'Judgement Entered': ['RJI', 'FSC Released', 'First FSC'],
       'Sale Held': ['RJI', 'FSC Released', 'First FSC'],
-      'Post-Sale Deed Recording': ['RJI', 'FSC Released', 'First FSC']
+      'Post-Sale Deed Recording': ['RJI', 'FSC Released', 'First FSC'],
     };
     this.leftSelectedItem = 'Case Info';
     // this.leftSelectedItem = 'Court Dates';
     // this.leftSideChange('Court Dates')
-    this.leftSideChange('Case Info')
+    this.leftSideChange('Case Info');
   }
 
   leftSideChange(data: any): void {
@@ -139,9 +198,9 @@ export class StartCaseComponent implements OnInit {
     if (data === 'Case Progress') {
       this.rightNavOpenStatus = true;
       this.rightSelectedItem = this.rightSideItems[0];
-      this.rightSideChange(this.rightSelectedItem)
+      this.rightSideChange(this.rightSelectedItem);
     } else {
-      this.rightNavOpenStatus = false ;
+      this.rightNavOpenStatus = false;
     }
   }
   rightSideChange(data: any): void {
@@ -152,7 +211,7 @@ export class StartCaseComponent implements OnInit {
     });
   }
 
-  findIndexById(id: string, data:any[]): number {
+  findIndexById(id: string, data: any[]): number {
     let index = -1;
     for (let i = 0; i < data.length; i++) {
       if (data[i].id === id) {
@@ -166,17 +225,26 @@ export class StartCaseComponent implements OnInit {
   openNew(id: string = ''): void {
     this.selectedCourtDate = {};
     if (id) {
-      this.selectedCourtDate = this.courtDates[this.findIndexById(id, this.courtDates)];
-      console.log(this.selectedCourtDate)
+      this.selectedCourtDate =
+        this.courtDates[this.findIndexById(id, this.courtDates)];
+      console.log(this.selectedCourtDate);
       // this.selectedCourtDate.createdDate = (typeof this.selectedCourtDate?.createdDate === 'object') ? this.selectedCourtDate?.createdDate?.toDate() : new Date();
       // this.selectedCourtDate.courtDate = this.selectedCourtDate?.courtDate?.toDate() || new Date();
-      this.selectedCourtDate.createdDate = new Date()
-      this.selectedCourtDate.courtDate = new Date()
+      this.selectedCourtDate.createdDate = new Date();
+      this.selectedCourtDate.courtDate = new Date();
     }
     this.submitted = false;
     this.courtDateDialog = true;
-    this.selectedCourtDate.createdDate = new FormControl(this.selectedCourtDate.createdDate ? (this.selectedCourtDate.createdDate) : new Date()).value;
-    this.selectedCourtDate.courtDate = new FormControl(this.selectedCourtDate.courtDate ? (this.selectedCourtDate.courtDate) : new Date()).value;
+    this.selectedCourtDate.createdDate = new FormControl(
+      this.selectedCourtDate.createdDate
+        ? this.selectedCourtDate.createdDate
+        : new Date()
+    ).value;
+    this.selectedCourtDate.courtDate = new FormControl(
+      this.selectedCourtDate.courtDate
+        ? this.selectedCourtDate.courtDate
+        : new Date()
+    ).value;
   }
 
   hideDialog() {
@@ -184,29 +252,42 @@ export class StartCaseComponent implements OnInit {
     this.submitted = false;
   }
 
-
   async saveCourtDate() {
     this.submitted = true;
     if (this.selectedCourtDate.id) {
-      this.courtDates[this.findIndexById(this.selectedCourtDate?.id, this.courtDates)] = this.selectedCourtDate;
-      this.messageService.add({severity:'success', summary: 'Successful', detail: 'Court Date Updated', life: 3000});
-    }
-    else {
+      this.courtDates[
+        this.findIndexById(this.selectedCourtDate?.id, this.courtDates)
+      ] = this.selectedCourtDate;
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'Court Date Updated',
+        life: 3000,
+      });
+    } else {
       this.selectedCourtDate.id = this.createId();
       this.courtDates.push(this.selectedCourtDate);
-      this.messageService.add({severity:'success', summary: 'Successful', detail: 'A new Court Date Created', life: 3000});
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'A new Court Date Created',
+        life: 3000,
+      });
     }
-    await this.store.collection(`Matters/`).doc(`${this.userId}`).update({courtDates: this.courtDates});
+    await this.store
+      .collection(`Matters/`)
+      .doc(`${this.userId}`)
+      .update({ courtDates: this.courtDates });
     this.courtDates = [...this.courtDates];
     this.courtDateDialog = false;
     this.selectedCourtDate = {};
   }
 
-
   createId(): string {
     let id = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for ( var i = 0; i < 5; i++ ) {
+    var chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 5; i++) {
       id += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return id;
